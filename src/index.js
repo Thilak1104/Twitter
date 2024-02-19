feb4969983974e5808151d02a5f4fbd9d97da492
Thilak <thilaksaithipparapu6300@gmail.com>
2024-02-19 21:50:39 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
 
 app.listen(3001, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
-   
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.create({content: 'With hooks now'});
-    console.log(tweet);
-    // const tweet = await tweetRepo.getAll(0,4);
-    // console.log(tweet[0].contentWithEmail);
 });
