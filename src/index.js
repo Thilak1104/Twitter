fc5f8a95c478b7ec7f6cefce2e1655132d4be47a
Thilak <thilaksaithipparapu6300@gmail.com>
2024-02-19 19:37:45 +0530

    Added Requirements

--- src/index.js
+++ src/index.js
@@ -9,25 +9,10 @@ app.listen(3001, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
-    // const tweet = await Tweet.create({
-    //     content:'Third tweet',
-        
-    // });
-    // const tweets = await Tweet.find();
-    // const tweet = await Tweet.findById('65ba5fcd8633329b5e392da8');
-    // tweet.userEmail = 'b@c.com';
-    // await tweet.save();
+   
     const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.create({content: ' tweet with a comment'});
-    // console.log(tweet);
-    // tweet.comments.push({content:'first comment'});
-    // await tweet.save();
-    // console.log(tweet);
-    const tweet = await tweetRepo.getAll(0,4);
-    // console.log(tweet);
-    // const comment = await Comment.create({content: 'new comment'});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-
-    console.log(tweet[0].contentWithEmail);
+    const tweet = await tweetRepo.create({content: 'With hooks now'});
+    console.log(tweet);
+    // const tweet = await tweetRepo.getAll(0,4);
+    // console.log(tweet[0].contentWithEmail);
 });
