f92ac04bbd9a1b889550e96b1c30e60a0cd01935
Thilak <thilaksaithipparapu6300@gmail.com>
2024-02-20 19:37:41 +0530

    Added lik model and basic user model

--- src/index.js
+++ src/index.js
@@ -11,13 +11,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-import services from './services/tweet-service.js'
+
 
 app.listen(3001, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
-    let ser = new services();
-    await ser.create({content: 'my other #CODe #works or #NOT ?'})
-
 });
